9c3dcd6a0d4aeb601428f5f7182f21eff1262d2e
nidhi22617 <pnidhi2@hotmail.com>
2024-10-07 01:32:29 +0545

    complete stylor

--- Awesome01/App.tsx
+++ Awesome01/App.tsx
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import FlatCards from './components/FlatCards';
 import ElevatedCards from './components/ElevatedCards';
 import FancyCards from './components/FancyCards';
+import ContactList from './components/ContactList';
 
 const App = () => {
   return (
@@ -11,6 +12,7 @@ const App = () => {
       <ScrollView>
         <FlatCards />
         <ElevatedCards/>
+        <ContactList/>
         <FancyCards/>
       </ScrollView>
     </SafeAreaView>
